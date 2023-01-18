ca8c533c4c65e0905eb0a0adc9f64aac87917571
dstenv <2973581533@qq.com>
2023-01-18 21:13:37 +0800

    format: 部分文件;fix: apis/base.ts

--- src/apis/base.ts
+++ src/apis/base.ts
@@ -1,22 +1,19 @@
-import { useBaseStore } from "@/stores/base";
-import pinia from "@/utils/createPinia";
-import axios from "axios";
-
-const baseStore = useBaseStore(pinia);
+import axios from 'axios'
+import { baseUrl } from '@/utils/config'
 
 const requestBaseConfig = {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-    url: "",
-};
+    method: 'get',
+    headers: { 'Content-Type': 'application/json' },
+    url: '',
+}
 
-type Request = typeof requestBaseConfig;
+type Request = typeof requestBaseConfig
 
 export const request = async <T>(options: T & Request) => {
-    options.method = options.method || requestBaseConfig.method;
-    options.headers = options.headers || requestBaseConfig.headers;
-    options.url += baseStore.baseUrl;
+    options.method = options.method || requestBaseConfig.method
+    options.headers = options.headers || requestBaseConfig.headers
+    options.url += baseUrl
 
-    const result = await axios({ ...options });
-    console.log("apis/base.ts请求结果 --> ", result);
-};
+    const result = await axios({ ...options })
+    console.log('apis/base.ts请求结果 --> ', result)
+}
