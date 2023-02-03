af04fb5aee54dc863e0229d54ac380651827bc93
dstenv <2973581533@qq.com>
2023-02-03 18:29:47 +0800

    change: src/apis/base.ts,增加超时时间

--- src/apis/base.ts
+++ src/apis/base.ts
@@ -1,30 +1,44 @@
 import axios from 'axios'
 import { baseConfig } from '@/utils/config'
 
+type Method = 'GET' | 'POST'
+type Content = 'application/json'
+
 export interface RequestBaseType {
     method?: Method
     headers?: {
         'Content-Type': Content
+        'Accept': Content
+        'Authorization': string
     }
     url?: string
     data?: any
     param?: any
+    timeout?: number
 }
-
-type Method = 'GET' | 'POST'
-type Content = 'application/json'
 
 const requestBaseConfig: RequestBaseType = {
     method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': '',
+    },
     url: '',
+    timeout: 5000,
 }
 
 export const request = async <T extends RequestBaseType, U>(
     options: T
 ): Promise<U> => {
     options.method = options.method || requestBaseConfig.method
-    options.headers = options.headers || requestBaseConfig.headers
+    if (options.headers) {
+        options.headers = {
+            ...requestBaseConfig.headers,
+            ...options.headers,
+        }
+    }
+    options.timeout = options.timeout || requestBaseConfig.timeout
     options.url = `/apis/${baseConfig.orgName}/${baseConfig.appName}/${options.url}`
 
     const result: U = await axios({ ...options })
